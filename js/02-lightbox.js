0893c80b8ab9cfa0124e8dd722470ec9d80899de
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2023-08-08 17:31:43 +0100

    hw7

---
+++ js/02-lightbox.js
@@ -0,0 +1,21 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryEl.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
